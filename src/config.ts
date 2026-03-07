b55d8e1397da3539196557f3fc5eb07f85dfb7fb
roary <rrichglitch@gmail.com>
2026-03-07 00:52:36 -0500

    id fix

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SPACETIMEDB_MODULE = 'repsoc';
 // SpacetimeAuth configuration - UPDATE THIS AFTER CREATING AUTH PROJECT
 export const AUTH_CONFIG = {
   authority: 'https://auth.spacetimedb.com/oidc',
-  client_id: '216469764654-m3p9i932n0rafahq0i2pcn5atfq0hqfn.apps.googleusercontent.com',
+  client_id: 'client_032dcrU7dNeqH21pwTabNC',
   redirect_uri: `${APP_URL}${BASE_PATH}/callback`,
   post_logout_redirect_uri: APP_URL,
   scope: 'openid profile email',
