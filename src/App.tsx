742d680717fdd6271ef31a860b364e222fa8cba2
roary <rrichglitch@gmail.com>
2026-03-08 18:37:30 -0400

    Fix: wrap /register in PrivateRoute to provide context

--- src/App.tsx
+++ src/App.tsx
@@ -199,8 +199,15 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
       <Route path="/callback" element={<CallbackPage />} />
+      <Route
+        path="/register"
+        element={
+          <PrivateRoute>
+            <RegisterPage />
+          </PrivateRoute>
+        }
+      />
       <Route
         path="/"
         element={
